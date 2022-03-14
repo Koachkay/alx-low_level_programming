750769178b18fe41b9e26d37ef004e8bae244371
root <tobiaskingsley12@gmail.com>
2022-03-13 20:58:51 -0700

    my first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
 /**
  * main - Entery point
  *
- *   * Return: Always 1
+ * Return: Always 1
  */ 
  int main(void)
 {
-		fprintf(stderr,
-			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-			return (1);
+	fprintf(stderr,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }

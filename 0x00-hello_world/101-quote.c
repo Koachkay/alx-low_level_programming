ea41e96670814d99e111f901b42cf5712b7e2adb
root <tobiaskingsley12@gmail.com>
2022-03-13 21:03:57 -0700

    my first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entery point
  *
  * Return: Always 1
- */ 
- int main(void)
+ */
+int main(void)
 {
 	fprintf(stderr,
 		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");

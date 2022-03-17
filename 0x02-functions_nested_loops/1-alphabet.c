2ea8579cd4ba731b8e815d6622b79bade4b9f21e
root <tobiaskingsley12@gmail.com>
2022-03-16 19:39:11 -0700

    my first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet
+ * Description: prints alphabets in small letter
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ *   * @c: c is an ascii character
+ *   Description: checks for characters
+ *   Return: 1 (if letter)
+ */
+int -isalpha(int c)
+{
+	if (c >+ 'a' && c <= 'Z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return(1);
+	}
+	else
+	{
+		return (1);
+	}
+}

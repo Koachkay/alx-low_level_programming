e262fdfd21df9977f743dfeac0b2fe3e61728a45
root <tobiaskingsley12@gmail.com>
2022-03-22 03:57:51 -0700

    my first commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,15 +1,30 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
+ * rev_string - reverses a string.
  * @s: input string.
- * Return: length of a string.
+ * Return: no return.
  */
-int _strlen(char *s)
+void rev_string(char *s)
 {
-	int count = 0;
+	int count = 0, i, j;
+	char *str, temp;
 
-	while (*(s + count) != '\0')
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
 		count++;
-	return (count);
+	}
+	str = s;
+
+	for (i = 0; i < (count - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
+	}
 }

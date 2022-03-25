d57c78bc57d7327d5a442404c5550c83c6954914
root <tobiaskingsley12@gmail.com>
2022-03-24 19:31:55 -0700

    my first commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,11 +16,11 @@ char *_strcat(char *dest, char *src)
 
 	while (count2 >= 0)
 	{
-		*(dest + count) = *(src + count2)/;
+		*(dest + count) = *(src + count2);
 		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
+		break;
+	count++;
+	count2++;
 	}
 	return (dest);
 }

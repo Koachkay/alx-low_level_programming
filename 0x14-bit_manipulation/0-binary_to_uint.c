2beb4e82a46e27f50db027a4caf578e2d36ea969
root <tobiaskingsley12@gmail.com>
2022-04-28 18:03:08 -0700

    my commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,7 +6,7 @@
  *
  * Return: unsigned int with decimal value of binsry number, or 0 if error
  */
-Return: unsigned int with decimal value of binsry number, or 0 if error
+unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int num;

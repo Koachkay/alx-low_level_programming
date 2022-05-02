03cdd6b02315a45d4bfebcedced7d8ec4af0a83d
root <tobiaskingsley12@gmail.com>
2022-05-02 16:09:43 -0700

    my commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -27,9 +27,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	nrd = read(fd, buf, letters);
 	nwr = write(STDOUT_FILENO, buf, nrd);
-	
+
 	close(fd);
-	
+
 	free(buf);
 
 	return (nwr);
